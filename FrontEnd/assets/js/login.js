edc81477d54567110f692d9bfe481158f77ae247
Lucie Leroux <lucieleroux93@gmail.com>
2024-05-03 13:02:26 +0200

    Ajout de la page connexion

---
+++ FrontEnd/assets/js/login.js
@@ -0,0 +1,8 @@
+//Formulaire de connexion
+
+const email = document.querySelector("form #email")
+const password = document.querySelector("form #password")
+
+console.log(email,password)
+
+const form = document.querySelector("form")
