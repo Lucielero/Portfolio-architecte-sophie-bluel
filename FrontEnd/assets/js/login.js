556c3e667f785bf1c1095fed43d2d3ac4d7b1d8d
Lucie Leroux <lucieleroux93@gmail.com>
2024-05-17 10:20:32 +0200

    modifications du formulaire de connexion

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,8 +1,54 @@
-//Formulaire de connexion
+//Gestion du formulaire de connexion
 
-const email = document.querySelector("form #email")
-const password = document.querySelector("form #password")
+function loginformSubmission(event) {
+    event.preventDefault() //empêcher le reload de la page afin d'éviter des perte de données
+    console.log("Formulaire soumis")
+    const emailInput = document.getElementById("email")
+    const passwordInput = document.getElementById("password")
+    //console.log(email,password)
 
-console.log(email,password)
+    //Récupérer les éléments notés par l'utilisateur
+    const email = emailInput.value.trim() //trim = retirer les éventuels espaces vides 
+    const password = passwordInput.value.trim()
+    console.log("Email:", email);
+    console.log("Password:", password);
 
-const form = document.querySelector("form")
+    //Requête à l'API
+    console.log("Envoi de la requête à l'API...")
+    fetch ("http://localhost:5678/api/users/login", {
+        method:"POST", //envoi des données
+        //format :
+        headers: {
+            "Content-Type": "application/json"
+        },
+        //corps de la requête :   
+        body: JSON.stringify({email,password})
+    })
+       
+    .then(response => {
+        console.log("Réponse de l'API reçue");
+        if (!response.ok) {
+            throw new Error("Une erreur est survenue lors de la requête")
+        }
+        return response.json()
+    })
+
+    .then(data => {
+        console.log("Données de réponse de l'API:", data)
+        if (data && data.token) {
+            //Redirection page d'accueil 
+            console.log("authentification réussie")
+            window.location.href = "../index.html"
+
+        } else {
+            console.log("Echec autentification")
+            //Affichage message d'erreur mauvais identifiants ou mdp
+            throw new Error("Erreur dans l'identifiant ou le mot de passe")
+        }
+    })
+}
+
+const loginForm = document.getElementById("login-form")
+const submitButton = document.getElementById("submit-button")
+
+submitButton.addEventListener("submit", loginformSubmission)
